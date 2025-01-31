e7686a1af565fe8cf11c15e0098a564bdf3bca7e
Haidar <afiq.haidar12@gmail.com>
2025-01-31 10:24:37 +0700

    feat: scipt cconverter

---
+++ script.py
@@ -0,0 +1,10 @@
+list event = read csv
+
+filter by webhist using pandas
+
+ambil 5 baris pertama dari list event
+
+unique message = get unique message string
+
+state = convert each unique message to variable A, B, C..
+
